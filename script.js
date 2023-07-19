91c671926c329aeac3556808d7d6eeca83bad58a
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-19 12:51:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
+let button = document.getElementById("swap");
+let main = document.querySelector(".day");
+button.addEventListener('click', swapTheme);
+function swapTheme(){
+	main.className =(main.className == "night"?"day":"night");
+	button.className = (button.className == "button_night"? "day": "button_night")
+}
+
+
+
 
